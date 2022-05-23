cc686bac37477c4e5d6bbaed91f938c362a50ae1
Andrew James Rasmussen <andrewras93@gmail.com>
2022-05-23 13:00:13 +0200

    Added helmet module.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const app = express();
 const port = 3000;
+const helmet = require("helmet");
 
-let test = 'HALLO';
-
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(express.json());
