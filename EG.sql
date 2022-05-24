544d566e1fbd221ea628b6ccc3f81b2e415ed890
toby044 <tobiasnolesen@gmail.com>
2022-05-24 10:55:09 +0200

    Styling

---
+++ EG.sql
@@ -0,0 +1,317 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: May 16, 2022 at 09:32 AM
+-- Server version: 5.7.34
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `EG`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(8) NOT NULL,
+  `group_number` int(6) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `group_number`, `name`, `description`) VALUES
+(1, 4740, 'Supplerende produkter', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `local_location`
+--
+
+CREATE TABLE `local_location` (
+  `id` int(6) NOT NULL,
+  `location_value` int(5) NOT NULL,
+  `zipcode` int(5) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `country` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `local_location`
+--
+
+INSERT INTO `local_location` (`id`, `location_value`, `zipcode`, `city`, `address`, `country`) VALUES
+(1, 110, 6000, 'kolding', 'byggemarkedsvej 1', 'danmark'),
+(2, 200, 6000, 'kolding', 'byggemarkedsvej 1', 'danmark'),
+(3, 131, 6000, 'kolding', 'byggemarkedsvej 1', 'danmark'),
+(4, 135, 6000, 'kolding', 'byggemarkedsvej 1', 'danmark');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(8) NOT NULL,
+  `item_number` int(10) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(300) DEFAULT '-',
+  `price` float NOT NULL DEFAULT '999.99',
+  `currency` varchar(3) NOT NULL DEFAULT 'DKK',
+  `unittype` enum('stk','fl','dås','sp','æsk','bæg','bøt','brv','dk','krt','lgd','m','pk','ps','ptr','rl','sk','tb') NOT NULL DEFAULT 'stk',
+  `price_ex_vat` tinyint(1) NOT NULL DEFAULT '0',
+  `discount` tinyint(1) NOT NULL DEFAULT '0',
+  `note` varchar(300) DEFAULT NULL,
+  `available_status` tinyint(1) NOT NULL,
+  `inventory_quantity` int(6) DEFAULT NULL,
+  `amount_in_order` int(10) DEFAULT NULL,
+  `min_inventory` int(5) DEFAULT NULL,
+  `supplier` varchar(25) NOT NULL,
+  `category` int(6) NOT NULL,
+  `local_location` int(6) DEFAULT NULL,
+  `sequence_number` int(6) NOT NULL,
+  `remote_location` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `item_number`, `title`, `description`, `price`, `currency`, `unittype`, `price_ex_vat`, `discount`, `note`, `available_status`, `inventory_quantity`, `amount_in_order`, `min_inventory`, `supplier`, `category`, `local_location`, `sequence_number`, `remote_location`) VALUES
+(1, 2017043, 'CARAMBA RENSESPRAY', '-', 12.99, 'DKK', 'stk', 0, 0, NULL, 0, 0, 20, 25, 'DIYsupply', 4740, 110, 114, NULL),
+(2, 1521026, 'MILLARCO SEKUNDLIM 30ML', '-', 23.99, 'DKK', 'fl', 0, 0, NULL, 1, 2, 31, 25, 'DIYsupply', 4740, 200, 123, 193),
+(3, 5063215, 'PLASTI KOTE FUGEPEN GRÅ 5ML', '-', 31.99, 'DKK', 'stk', 0, 0, NULL, 1, 26, 31, 100, 'DIYsupply', 4740, 131, 36, NULL),
+(4, 3701612, 'PLASTIC PADDING 100 0,56LT', '-', 36.99, 'DKK', 'dås', 0, 0, NULL, 1, 2, 20, 15, 'DIYsupply', 4740, 135, 11, NULL),
+(9, 8527673, 'DANA GLASBÅND KISO 141 SORT', '-', 89.99, 'DKK', 'rl', 0, 0, NULL, 1, 37, 40, 50, 'DIYsupply', 4740, NULL, 3, 4123);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `remote_location`
+--
+
+CREATE TABLE `remote_location` (
+  `id` int(8) NOT NULL,
+  `remote_location` int(5) DEFAULT NULL,
+  `zipcode` int(5) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `country` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `remote_location`
+--
+
+INSERT INTO `remote_location` (`id`, `remote_location`, `zipcode`, `city`, `address`, `country`) VALUES
+(1, 4123, 7000, 'vejle', 'fjernlagervej 1', 'danmark'),
+(2, 193, 7000, 'vejle', 'fjernlagervej 1', 'danmark'),
+(3, 256, 7000, 'vejle', 'fjernlagervej 1', 'danmark');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` int(8) NOT NULL,
+  `name` varchar(75) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(75) NOT NULL,
+  `zipcode` int(5) NOT NULL,
+  `country` varchar(75) NOT NULL,
+  `contact` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `name`, `address`, `city`, `zipcode`, `country`, `contact`) VALUES
+(1, 'DIYsupply', 'suppliervejen 1', 'aalborg', 9000, 'danmark', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier_contact`
+--
+
+CREATE TABLE `supplier_contact` (
+  `id` int(4) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `phone` int(15) NOT NULL,
+  `position` varchar(30) DEFAULT NULL,
+  `supplier_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `supplier_contact`
+--
+
+INSERT INTO `supplier_contact` (`id`, `firstname`, `lastname`, `email`, `phone`, `position`, `supplier_id`) VALUES
+(1, 'Jens', 'Jensen', 'jensjensen@diysupply.dk', 12345678, 'keyaccount manager', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(8) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password_hashed` varchar(64) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `group_number` (`group_number`) USING BTREE;
+
+--
+-- Indexes for table `local_location`
+--
+ALTER TABLE `local_location`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `location_value` (`location_value`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `item_number` (`item_number`),
+  ADD KEY `belongs_to` (`category`),
+  ADD KEY `remote_location` (`remote_location`) USING BTREE,
+  ADD KEY `local_location` (`local_location`) USING BTREE,
+  ADD KEY `supplier` (`supplier`) USING BTREE;
+
+--
+-- Indexes for table `remote_location`
+--
+ALTER TABLE `remote_location`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `remote_location` (`remote_location`) USING BTREE;
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`);
+
+--
+-- Indexes for table `supplier_contact`
+--
+ALTER TABLE `supplier_contact`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_brand` (`supplier_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `local_location`
+--
+ALTER TABLE `local_location`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `remote_location`
+--
+ALTER TABLE `remote_location`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `supplier_contact`
+--
+ALTER TABLE `supplier_contact`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `belongs_to` FOREIGN KEY (`category`) REFERENCES `category` (`group_number`),
+  ADD CONSTRAINT `located_locally_at` FOREIGN KEY (`local_location`) REFERENCES `local_location` (`location_value`),
+  ADD CONSTRAINT `located_remotely_at` FOREIGN KEY (`remote_location`) REFERENCES `remote_location` (`remote_location`),
+  ADD CONSTRAINT `supplied_by` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`name`);
+
+--
+-- Constraints for table `supplier_contact`
+--
+ALTER TABLE `supplier_contact`
+  ADD CONSTRAINT `supplier` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
